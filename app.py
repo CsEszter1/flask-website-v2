620870568d659f77f083f0aeba5140c8c497760e
CsEszter1 <117866984+CsEszter1@users.noreply.github.com>
2023-04-20 14:42:03 +0000

    added HTML and CSS

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
-from flask import Flask  # from flask modul import Flask class 
+from flask import Flask, render_template  # from flask modul import Flask class 
 
 app = Flask(__name__)   # app is the object of the class flask, import flask functionality
 @app.route("/")
 def hello_world():
-  return "Hello, world"
+  return render_template('home.html')
 
 print(__name__)
 if __name__ == "__main__":
