4fdf91eefe7bf7787f14e4a4e32fc1bf0877f4cc
CsEszter1 <117866984+CsEszter1@users.noreply.github.com>
2023-04-21 13:28:54 +0000

    .

--- app.py
+++ app.py
@@ -1,9 +1,35 @@
-from flask import Flask, render_template  # from flask modul import Flask class 
+from flask import Flask, render_template, jsonify  # from flask modul import Flask class 
 
 app = Flask(__name__)   # app is the object of the class flask, import flask functionality
+
+JOBS = [
+  {
+    'id': 1,
+    'title': 'Research Assistant',
+    'location': 'Frankfurt, Germany',
+    'salary': '1000 euro'
+  },
+  {
+    'id': 2,
+    'title': 'Group Leader',
+    'location': 'Oslo, Norway',
+    
+  },
+  {
+    'id': 3,
+    'title': 'Research Fellow',
+    'location': 'Berlin., Germany',
+    'salary': '2200 euro'
+  }
+]
 @app.route("/")
-def hello_world():
-  return render_template('home.html')
+def hello_science():
+  return render_template('home.html', jobs=JOBS)
+
+@app.route("/api/jobs")
+def list_jobs():
+  return jsonify(JOBS)
+  
 
 print(__name__)
 if __name__ == "__main__":
