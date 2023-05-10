0a3606e6dfc60d17f815ca560f459f7a34705516
CsEszter1 <117866984+CsEszter1@users.noreply.github.com>
2023-05-10 21:33:17 +0200

    Update database.py

--- database.py
+++ database.py
@@ -1,3 +1,22 @@
-import sqlalchemy
+from sqlalchemy import create_engine, text
 
-print(sqlalchemy.__version__)
+db_connection_string = "mysql+pymysql://hed1x7hh71r7dhy9uvx1:pscale_pw_esfRX4BP3CCPuMOM53qe3RYzhQ2qo0x0mM9O3m9uJy2@aws.connect.psdb.cloud/researchcareers?charset=utf8mb4"
+
+engine = create_engine(
+  db_connection_string,
+  connect_args={
+      "ssl": {
+          "ssl_ca": "/etc/ssl/cert.pem"
+      }
+  })
+
+
+with engine.connect() as conn:
+  result = conn.execute(text("select * from jobs"))
+
+  result_dicts = []
+  for row in result.all():
+    result_dicts.append(row._mapping)
+
+  print(result_dicts)
+ 
