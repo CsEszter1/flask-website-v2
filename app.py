53936bc332ac8e76be8aa2b5367348a4d9ccec50
CsEszter1 <117866984+CsEszter1@users.noreply.github.com>
2023-05-14 16:04:52 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, jsonify  # from flask modul import Flask class 
-from database import load_jobs_from_db
+from database import load_jobs_from_db, load_job_from_db
 
 
 app = Flask(__name__)   # app is the object of the class flask, import flask functionality
@@ -17,6 +17,11 @@ def list_jobs():
   jobs = load_jobs_from_db()
   return jsonify(jobs)
   
+@app.route("/job/<id>")
+def show_job(id):
+  job = load_job_from_db(id)
+  return jsonify(job)
+
 
 print(__name__)
 if __name__ == "__main__":
